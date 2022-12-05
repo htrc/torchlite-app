99754e3655b884ad2bc168086ff08a6f013461ac
cwulfman <cwulfman@princeton.edu>
2022-12-05 16:39:48 -0500

    updates index test

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -6,7 +6,7 @@ describe('Home', () => {
     render(<Home />)
 
     const heading = screen.getByRole('heading', {
-      name: /welcome to Torchlite/i,
+      name: /Welcome to Torchlite/i,
     })
 
     expect(heading).toBeInTheDocument()
