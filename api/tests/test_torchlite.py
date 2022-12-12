9e91cd3e782c712981ef191cfe57fbc87ca6dc1f
cwulfman <cwulfman@princeton.edu>
2022-12-12 09:40:16 -0500

    adds test_torchlite.py

--- api/tests/test_torchlite.py
+++ api/tests/test_torchlite.py
@@ -1,24 +1,26 @@
 import pytest
-from torchlitelib import __version__
-from torchlitelib.extracted_features import WorkSet, Volume, Page
-
-def test_version():
-    assert __version__ == '0.1.0'
+from extracted_features.workset import WorkSet
+from extracted_features.page import Page
+from extracted_features.volume import Volume
 
 
 @pytest.fixture
 def volume_1():
     return Volume("loc.ark+=13960=t46q23w14")
 
+
 @pytest.fixture
 def page_1(volume_1):
     return volume_1.pages[0]
 
+
 def test_volume(volume_1):
     assert volume_1.id == "loc.ark+=13960=t46q23w14"
 
+
 def test_page(page_1):
     assert page_1.tokenCount == 52
+
 
 def test_volume_tokens(volume_1):
     tokens = volume_1.tokens
